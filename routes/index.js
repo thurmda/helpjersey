a6200203ea69643e50d79e85ff2c7607b79d0776
Dan Thurman <dan.thurman@buddymedia.com>
2012-11-01 00:19:00 -0400

    Moved majors UI to layout.ejs and CSS to helpjersey.css

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,27 @@
 
 exports.index = function(req, res){
-  res.render('index', { 
+  var page = 'index';
+  res.render(page, { 
         title: 'Help Jersey',
-        header: Math.floor((Math.random()*3)+1)
+        header: Math.floor((Math.random()*3)+1),
+        bodyClass: page
   });
 };
 
 exports.about = function(req, res){
-  res.render('about', {
-        title: 'About' 
-  })
+  var page = 'about';
+  res.render(page, {
+        title: 'About', 
+        header: Math.floor((Math.random()*3)+1),
+        bodyClass: page
+  });
 };
+
+exports.contact = function(req, res){
+  var page = 'contact';
+  res.render(page, {
+        title: 'Contact Us', 
+        header: Math.floor((Math.random()*3)+1),
+        bodyClass: page
+  });
+}
