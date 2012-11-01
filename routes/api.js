44557c31e85ce2d6795665c1813a427061db07ac
Dan Thurman <dan.thurman@buddymedia.com>
2012-11-01 10:39:07 -0400

    API and ajax

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,7 @@
 var municipal = require('../data/municipal');
 
 exports.municipal = function(req, res){
-
-res.writeHead(200, {"Content-Type": "application/json"});
-
-res.write(
-    JSON.stringify(municipal)
-); 
-  //res.render(page, { 
-        //title: 'Help Jersey',
-        //layout: 'layouts/default',
-        //header: Math.floor((Math.random()*3)+1),
-        //bodyClass: page,
-  //});
-};
-
-
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.write(JSON.stringify(municipal));
+    res.end();
+}
