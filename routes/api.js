63468ba04b2f50ae828ccd2b9980e08b303a2ad6
Dan Thurman <dan.thurman@buddymedia.com>
2012-11-02 04:02:39 -0400

    Search and Find with #! URls for linking... Save updates coming next

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,21 @@
-var municipal = require('../data/municipal');
 var ourTils = require('../lib/ourTils');
 
-exports.municipal = function(req, res){
-    ourTils.find({}, {}, function(err, cursor) {
+exports.allTowns = function(req, res){
+    ourTils.find({}, {name:1, _id:0}, function(err, cursor) {
+                 if (err) {
+                    console.error(err);
+                    return;
+                 }
+
+                cursor.sort({name: 1}).toArray(function(err,a){
+                    res.writeHead(200, {"Content-Type": "application/json"});
+                    res.write(JSON.stringify(a));
+                    res.end();
+                });
+    });
+}
+exports.town = function(req, res){
+    ourTils.find({name : req.query.town}, {}, function(err, cursor) {
                  if (err) {
                     console.error(err);
                     return;
