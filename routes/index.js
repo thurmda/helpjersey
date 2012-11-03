1533499288edb3b2668da143e5b6a6413350a3b8
Dan Thurman <dan.thurman@buddymedia.com>
2012-11-03 01:59:38 -0400

    Data!

--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,74 @@
+function Page(name){
+    return {
+            name: name,
+            title: 'Help Jersey',
+            layout: 'layouts/minimal',
+            header: Math.floor((Math.random()*3)+1),
+            description: '',
+            bodyClass: name
+        };
+}
 
 exports.index = function(req, res){
-  var page = 'index';
-  res.render(page, { 
-        title: 'Help Jersey',
-        layout: 'layouts/default',
-        header: Math.floor((Math.random()*3)+1),
-        bodyClass: page
-  });
+  var page = Page('index');
+      page.layout = 'layouts/default';
+  res.render(page.name, page);
 };
 
-exports.about = function(req, res){
-  var page = 'about';
-  res.render(page, {
-        title: 'About', 
-        layout: 'layouts/default',
-        header: Math.floor((Math.random()*3)+1),
-        bodyClass: page
-  });
+exports.info = function(req, res){
+  var page = Page('info');
+      page.title = 'Information';
+  res.render(page.name,page);
 };
 
-exports.contact = function(req, res){
-  var page = 'contact';
-  res.render(page, {
-        title: 'Contact Us', 
-        layout: 'layouts/default',
-        header: Math.floor((Math.random()*3)+1),
-        bodyClass: page
-  });
-}
 
-exports.donate = function(req, res){
-  var page = 'donate';
-  res.render(page, {
-        title: 'Donate', 
-        layout: 'layouts/donate',
-        header: Math.floor((Math.random()*3)+1),
-        bodyClass: page
-  });
-}
+exports.need = function(req, res){
+  var page = Page('need');
+      page.title = 'Need Help';
+  res.render(page.name,page);
+};
+
+exports.give = function(req, res){
+  var page = Page('give');
+      page.title = 'Give Help';
+  res.render(page.name, page);
+};
 
 exports.map = function(req, res){
-  var page = 'map';
-  res.render(page, {
-        title: 'The Jersey Shore', 
-        layout: 'layouts/default',
-        header: Math.floor((Math.random()*3)+1),
-        bodyClass: page
-  });
+  var page = Page('map');
+      page.title = 'New Jersey';
+  res.render(page.name, page);
 }
+
+
+//exports.about = function(req, res){
+//  var page = 'about';
+//  res.render(page, {
+//        title: 'About', 
+//        layout: 'layouts/default',
+//        header: Math.floor((Math.random()*3)+1),
+//        bodyClass: page
+//  });
+//};
+
+//exports.contact = function(req, res){
+//  var page = 'contact';
+//  res.render(page, {
+//        title: 'Contact Us', 
+//        layout: 'layouts/default',
+//        header: Math.floor((Math.random()*3)+1),
+//        bodyClass: page
+//  });
+//}
+
+//exports.donate = function(req, res){
+//  var page = 'donate';
+//  res.render(page, {
+//        title: 'Donate', 
+//        layout: 'layouts/donate',
+//        header: Math.floor((Math.random()*3)+1),
+//        bodyClass: page
+//  });
+//}
 
 
